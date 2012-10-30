b588e9052b4137f27ee74c1972c6fe9ae75342b9
Web Advanced <paul.barton@webadvanced.com>
2012-10-30 11:05:40 -0700

    added HoneypotData tests

---
+++ src/SimpleHoneypot.Tests/HoneypotDataTests.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleHoneypot.Tests {
+    using System;
+
+    using SimpleHoneypot.Core;
+
+    using Xunit;
+
+    public class HoneypotDataTests {
+        #region Public Methods and Operators
+
+        [Fact]
+        public void Ctor_ShouldSetInputNameValue_WhenPassedValidString() {
+            var honeypotData = new HoneypotData("Valid String");
+            Assert.Equal(honeypotData.InputNameValue, "Valid String");
+        }
+
+        [Fact]
+        public void Ctor_ShouldThrowArgumentException_WhenPassedNullOrEmpty() {
+            Assert.Throws<ArgumentException>(() => new HoneypotData(null));
+            Assert.Throws<ArgumentException>(() => new HoneypotData(string.Empty));
+        }
+
+        #endregion
+    }
+}
